f2ed273780aa4272d4897f0abf3ba7ef02ac0711
Katharina Winkler <kasowi@outlook.com>
2022-04-19 14:38:12 +0200

    very basic filter as you type

--- src/Components/CharacterCard.tsx
+++ src/Components/CharacterCard.tsx
@@ -1,5 +1,6 @@
 import {Character} from "../Model/Character";
 import "./CharacterCard.css";
+import React, {useState} from "react";
 
 type characterCardProps ={
     character: Character
@@ -7,6 +8,12 @@ type characterCardProps ={
 }
 
 export default function CharacterCard({character} :characterCardProps){
+
+    const [count, setCount] = useState<number>(0);
+    const increaseCount =  () => {
+        setCount(count + 1);
+    }
+
     return<div className={"CCard"}>
         <div className={"CCImage"}><img src={character.image}/></div>
         <div className={"CCName"}><h2>{character.name}</h2></div>
@@ -14,6 +21,12 @@ export default function CharacterCard({character} :characterCardProps){
         <div className={"CCOrigin"}><b>Origin: </b>{character.origin.name}</div>
         <div className={"CCSpecies"}><b>Species: </b>{character.species}</div>
         <div className={"CCStatus"}><b>Status: </b>{character.status}</div>
+        <div className ="LikeButton">
+            <button onClick={increaseCount}>
+                Like
+            </button>
+            <p> {count} likes</p>
+        </div>
     </div>
 
 }
---
+++ src/Components/Filter.tsx
@@ -0,0 +1,11 @@
+
+export default function FilterBy () {
+
+    return (
+        <div>
+
+
+        </div>
+    )
+
+}
